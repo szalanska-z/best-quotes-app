739d437a7837c7613dcb0c7a0127fbff265288d2
Zaneta Szalanska <zaneta.szalanska@gmail.com>
2020-09-02 12:50:59 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Quotation } from './models/quotation';
+import { QUOTES } from './models/data-base';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'best-quotes-app';
+  showForm = false;
+
+  quotes: Quotation[] = QUOTES;
+
+  quotation: Quotation = {
+    author: '',
+    sentence: '',
+    votes: 0,
+  };
+
+  value = 1;
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
+
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = {
+      author: '',
+      sentence: '',
+      votes: 0,
+    };
+  }
+
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += this.value;
+  }
+
+  removeVote(quotation: Quotation, value: number) {
+    quotation.votes -= this.value;
+  }
 }
