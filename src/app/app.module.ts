c693e6d76a9d327ec98813dfe0df25984067f9b6
Zaneta Szalanska <zaneta.szalanska@gmail.com>
2020-09-03 14:13:30 +0200

    create button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { FormComponent } from './form/form.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, RankingComponent, FormComponent],
+  declarations: [AppComponent, NavbarComponent, RankingComponent, FormComponent, ButtonComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent implements OnInit {
+  @Output()
+  clickButton = new EventEmitter<void>();
+  @Input()
+  customClasses: string;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
